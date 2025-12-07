87fe9dc45a3fc4cd1daa444fa385ada3ef29f3d2
JeromeGsq <jerome.ghesquiere@gmail.com>
2025-12-07 20:23:40 +0100

    Add `motor` dependency and update UI components
    - Introduced `motor` package version `1.1.0` in `pubspec.yaml` and updated `pubspec.lock`.
    - Refactored `NDSOverlay` widget to improve overlay functionality and visual behavior.
    - Updated image asset `button_more_apps.png` to enhance UI consistency.

--- lib/widgets/overlay.dart
+++ lib/widgets/overlay.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:motor/motor.dart';
 import 'package:nds_gui_kit/services/overlay_service.dart';
 import 'package:signals/signals_flutter.dart';
 
 class NDSOverlay extends StatelessWidget {
-  const NDSOverlay({super.key, this.child});
+  const NDSOverlay({super.key, required this.child});
 
-  final Widget? child;
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +14,26 @@ class NDSOverlay extends StatelessWidget {
       final isVisible = overlaySignal.value;
 
       return Stack(
+        fit: StackFit.expand,
         children: [
-          if (child != null) Positioned.fill(child: child!),
-          if (isVisible)
-            GestureDetector(
-              onTap: hideOverlay,
-              child: Container(color: Colors.black.withValues(alpha: 1)),
+          child,
+          IgnorePointer(
+            ignoring: !isVisible,
+            child: SingleMotionBuilder(
+              motion: CupertinoMotion.snappy(),
+              value: isVisible ? 1.0 : 0.0,
+              builder: (context, opacity, child) {
+                if (opacity == 0.0) return const SizedBox.shrink();
+
+                return GestureDetector(
+                  onTap: hideOverlay,
+                  child: Container(
+                    color: Colors.black.withValues(alpha: opacity),
+                  ),
+                );
+              },
             ),
+          ),
         ],
       );
     });
